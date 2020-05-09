e88b5eb45eded2b934674b6ef611ba434e9035d4
Nazar Sekh <nazar.sekh@gmail.com>
2020-05-09 22:52:15 +0300

    f

---
+++ src/app/home/user-account/project-status-one/project-status-one.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-status-one',
+  templateUrl: './project-status-one.component.html',
+  styleUrls: ['./project-status-one.component.css']
+})
+export class ProjectStatusOneComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
